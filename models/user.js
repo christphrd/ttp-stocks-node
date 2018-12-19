c674265600d515bba96cd45f67718a19f1f57c42
christphrd <diep.christopher@gmail.com>
2018-12-19 05:14:41 -0500

    index uniq on email

--- models/user.js
+++ models/user.js
@@ -1,11 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const User = sequelize.define('User', 
+  {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     account_balance: DataTypes.DECIMAL,
     password_digest: DataTypes.STRING
-  }, {});
+  }, 
+  {
+    indexes: [
+      // Create a unique index on email
+      {
+        unique: true,
+        fields: ['email']
+      },
+    ]
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
