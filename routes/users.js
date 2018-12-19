30154bcb842df68bd6a0a73ccc864fe305baa510
christphrd <diep.christopher@gmail.com>
2018-12-18 22:47:43 -0500

    test users listing

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var models = require("../models");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const allUsers = await models.User.findAll()
+  res.send(allUsers);
 });
 
 module.exports = router;
