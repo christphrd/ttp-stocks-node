13ececdb7925ea295ca62681ff3ec5226cd7f9d1
christphrd <diep.christopher@gmail.com>
2018-12-20 23:43:21 -0800

    error message sent for API call

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.options('/register', cors()) // enable pre-flight request for DELETE request
+router.options('/register', cors()) // enable pre-flight request for POST request
 router.post('/register', async function(req, res, next) {
-  let newAcc = await models.User.newAccount(req.body)
+  let newAcc = await models.User.newAccount(req.body);
+
   newAcc.save()
   .then(saved => {
     let json = { status: 201, message: "Successfully created.", user: { email: saved.email, name: saved.name, account_balance: saved.account_balance }, token: null }
     res.json(json)
   })
-  .catch(console.log)
+  .catch(err => res.status(400).json({errors: err.errors[0].message}))
 })
 
 module.exports = router;
